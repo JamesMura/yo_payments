04d7d71a5040f663734db1640b591755da698715
James Muranga <jmured@gmail.com>
2015-02-25 22:19:55 +0300

    changes to enable the 0.13 release

--- setup.py
+++ setup.py
@@ -39,10 +39,10 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: MIT License',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
     ],
-    test_suite='tests',
+    test_suite='tests'
 )
